252ef301550d47edeaf3a8ca91183e199778a6f9
CavidResulov <cavid.resulov@hotmail.com>
2021-10-18 19:47:01 +0400

    js tasks

---
+++ javascript/app.js
@@ -0,0 +1,19 @@
+// dordreqemli ededin reqemleri cemi
+
+var value = 1298,
+  sum = 0;
+
+while (value) {
+  sum += value % 10;
+  value = Math.floor(value / 10);
+}
+
+console.log(sum);
+
+// dordreqemli ededin reqemleri cemi
+
+// prompt ad yazdir
+
+let person = prompt("Adinizi daxil edin", "");
+
+// prompt ad yazdir
